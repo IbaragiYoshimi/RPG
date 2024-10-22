04133200f43aecada9d90428f5111021cd409865
TomLiszt <spectralsteedwhistle@gmail.com>
2024-10-22 08:57:23 +0800

    Save regularly
    Up to Lesson 50. Enemy's battle state.

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,18 +4,78 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    public virtual void Awake()
+    [Header("Collision info")]
+    [SerializeField] protected Transform groundCheck;
+    [SerializeField] protected float groundCheckDistance;
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected LayerMask whatIsGround;        // 碰撞检测时需要知道哪些层算“碰上了”。
+
+    public int facingDir { get; private set; } = 1;         // 注意，facingDir 的初始值一定要设置，不然默认就是 0。会导致冲刺时速度变为 0。
+    protected bool facingRight = true;
+
+    #region Components
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    #endregion
+
+    protected virtual void Awake()
     {
 
     }
 
-    public virtual void Start()
+    protected virtual void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
     {
 
     }
 
-    public virtual void Update()
-    {
+    #region Velocity
+    // 快速设置角色速度为(0, 0)
+    public void ZeroVelocity() => rb.velocity = new Vector2(0, 0);
 
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);     // 将反转图像的控制器放在这里，避免主 update 循环中杂乱无章。反正 SetVelocity 就是设置速度的，逻辑上说得通。
     }
+    #endregion
+
+    #region Collision
+    public virtual bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);     // 射线起点，射线方向，射线长度，射线终点标志。
+    public virtual bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+    #endregion
+
+    #region Flip
+    public virtual void Flip()
+    {
+        facingDir = facingDir * -1;     // 将这两个标志位反转。
+        facingRight = !facingRight;
+
+        transform.Rotate(0, 180, 0);    // 将图像按 y 轴反转。
+    }
+
+    /* 
+     * 这个没搞懂，为什么需要 FlipController 来控制图像反转呢，岂不是多此一举。
+     */
+    public virtual void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+            Flip();
+        else if (_x < 0 && facingRight)
+            Flip();
+    }
+    #endregion
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
     [Header("Attack details")]
     public Vector2[] attackMovement;
@@ -19,21 +19,6 @@ public class Player : MonoBehaviour
     public float dashDuration;
     public float dashDir { get; private set; }
 
-    [Header("Collision info")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private float wallCheckDistance;
-    [SerializeField] private LayerMask whatIsGround;        // 碰撞检测时需要知道哪些层算“碰上了”。
-
-    public int facingDir { get; private set; } = 1;         // 注意，facingDir 的初始值一定要设置，不然默认就是 0。会导致冲刺时速度变为 0。
-    private bool facingRight = true;
-
-    #region Components
-    public Animator anim { get; private set; }
-    public Rigidbody2D rb { get; private set; }
-    #endregion
-
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
 
@@ -48,8 +33,9 @@ public class Player : MonoBehaviour
 
     #endregion
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         stateMachine = new PlayerStateMachine();        // 注意顺序，先实例化 stateMachine。
 
         /*实例化 idleState 时，调用其构造函数，将 this 指针传入，它通过 this 指针在自己的脚本内构造出 player，
@@ -67,15 +53,17 @@ public class Player : MonoBehaviour
         primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
 
-    private void Start()
+    protected override void Start()
     {
-        anim = GetComponentInChildren<Animator>();
+        base.Start();
+
         stateMachine.Initialize(idleState);
-        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
+
         stateMachine.currentState.Update();
 
         CheckForDashInput();
@@ -103,47 +91,5 @@ public class Player : MonoBehaviour
         }
     }
 
-    #region Velocity
-    // 快速设置角色速度为(0, 0)
-    public void ZeroVelocity() => rb.velocity = new Vector2(0, 0);
     
-
-    public void SetVelocity(float _xVelocity, float _yVelocity)
-    {
-        rb.velocity = new Vector2(_xVelocity, _yVelocity);
-        FlipController(_xVelocity);     // 将反转图像的控制器放在这里，避免主 update 循环中杂乱无章。反正 SetVelocity 就是设置速度的，逻辑上说得通。
-    }
-    #endregion
-
-    #region Collision
-    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);     // 射线起点，射线方向，射线长度，射线终点标志。
-    public bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
-    
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
-    }
-    #endregion
-
-    #region Flip
-    public void Flip()
-    {
-        facingDir = facingDir * -1;     // 将这两个标志位反转。
-        facingRight = !facingRight;
-
-        transform.Rotate(0, 180, 0);    // 将图像按 y 轴反转。
-    }
-
-    /* 
-     * 这个没搞懂，为什么需要 FlipController 来控制图像反转呢，岂不是多此一举。
-     */
-    public void FlipController(float _x)
-    {
-        if (_x > 0 && !facingRight)
-            Flip();
-        else if (_x < 0 && facingRight)
-            Flip();
-    }
-    #endregion
 }
