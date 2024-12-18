acfb149ea0f030b0583e4be5e86f4d2e24564330
TomLiszt <spectralsteedwhistle@gmail.com>
2024-12-19 02:05:58 +0800

    Save regularly.
    Up to Lesson 113. Player drop system.

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyStats : CharacterStats
 {
     private Enemy enemy;
+    private ItemDrop myDropSystem;
 
     [Header("Level details")]
     [SerializeField] private int level = 1;
@@ -19,6 +20,7 @@ public class EnemyStats : CharacterStats
         base.Start();
 
         enemy = GetComponent<Enemy>();
+        myDropSystem = GetComponent<ItemDrop>();
 
     }
 
@@ -65,5 +67,7 @@ public class EnemyStats : CharacterStats
         base.Die();
 
         enemy.Die();
+
+        myDropSystem.GenerateDrop();
     }
 }
--- Assets/Scripts/Items and Inventory/ItemObject.cs
+++ Assets/Scripts/Items and Inventory/ItemObject.cs
@@ -6,22 +6,31 @@ using UnityEngine;
  * 后者则是库存对象，用于包装可看作基本类型的 ItemData，主要是维护库存数量等信息，仅在后台计算中使用，并不会出现在 Scene 中。故不需要继承 Monobehaviour。 */
 public class ItemObject : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private ItemData itemData;
 
-    // 该函数是内置函数，在修改 gameObject 时会自动调用，用于验证正确性。
-    private void OnValidate()
+
+    private void SetupVisuals()
     {
+        if (itemData == null)
+            return;
+
         GetComponent<SpriteRenderer>().sprite = itemData.icon;
-        gameObject.name = "Item Object - " + itemData.name;
+        gameObject.name = "Item Object - " + itemData.itemName;
     }
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void SetupItem(ItemData _itemData, Vector2 _velocity)
     {
-        if (collision.GetComponent<Player>() != null)
-        {
-            Inventory.instance.AddItem(itemData);
-            Destroy(gameObject);
-        }
+        itemData = _itemData;
+        rb.velocity = _velocity;
+
+        SetupVisuals();
+    }
+
+    public void PickupItem()
+    {
+        Inventory.instance.AddItem(itemData);
+        Destroy(gameObject);
     }
 }
